0a939d730dde5c087ed98863b04aa296a77f265c
mv89-heb <valanimattan89@gmail.com>
2026-06-24 17:24:06 +0300

    Update config.py

--- config.py
+++ config.py
@@ -6,5 +6,7 @@ if database_url and database_url.startswith("postgres://"):
     database_url = database_url.replace("postgres://", "postgresql://", 1)
 
 class Config:
+    # כאן אנחנו מושכים את המפתח הסודי מ-Render
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'default-dev-key-123') 
     SQLALCHEMY_DATABASE_URI = database_url or 'sqlite:///db.sqlite3'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
