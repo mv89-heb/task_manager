479e010f4ceed3b43053322a131cf12a757fcc48
mv89-heb <valanimattan89@gmail.com>
2026-06-24 15:04:21 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,11 @@ class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "super-secret-key")
 
     DATABASE_URL = os.getenv("DATABASE_URL")
+
     if DATABASE_URL:
-        SQLALCHEMY_DATABASE_URI = DATABASE_URL.replace("postgres://", "postgresql://")
+        # ✅ FIX FOR RENDER
+        DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://")
+        SQLALCHEMY_DATABASE_URI = DATABASE_URL
     else:
         SQLALCHEMY_DATABASE_URI = "sqlite:///db.sqlite3"
 
