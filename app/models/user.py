15d02420554e0e5be3dd9285f2145e034bfed6a0
mv89-heb <valanimattan89@gmail.com>
2026-06-24 17:26:19 +0300

    Update user.py

--- app/models/user.py
+++ app/models/user.py
@@ -1,19 +1,9 @@
-from app import db, login_manager
+from app import db
 from flask_login import UserMixin
-import bcrypt
+from datetime import datetime
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True)
-    password = db.Column(db.String(200))
-    role = db.Column(db.String(20), default="USER")
-
-    def set_password(self, password):
-        self.password = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
-
-    def check_password(self, password):
-        return bcrypt.checkpw(password.encode(), self.password.encode())
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
