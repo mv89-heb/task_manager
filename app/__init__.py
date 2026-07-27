0b59b458addf239b906da91fe21d9aee721b4c5e
mv89-heb <valanimattan89@gmail.com>
2026-07-27 17:22:46 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -254,4 +254,4 @@ def create_app():
 @login_manager.user_loader
 def load_user(user_id):
     from app.models.user import User
-    return User.query.get(int(user_id))
+    return db.session.get(User, int(user_id))
